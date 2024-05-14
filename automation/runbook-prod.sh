9ec06c7fbbc1d68844dfc9dbab80bc783186bd41
Victor Grenu <victor.grenu@gmail.com>
2024-05-14 09:56:52 +0200

    better script and task

--- automation/runbook-prod.sh
+++ automation/runbook-prod.sh
@@ -1,64 +1,102 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Set variables
-date=$(date +%Y-%m-%d-%H-%M)
-wordToRemove="policies/"
-region="eu-west-1"
-repoUrl="git@github.com:z0ph/MAMIP.git"
-repoPath="/tmp/MAMIP"
-s3KeyPath="s3://mamip-artifacts/mamip"
-sshKeyPath="/tmp/mamip.key"
-gitUserName="MAMIP Bot"
-gitUserEmail="mamip_bot@github.com"
-snsTopicArn="arn:aws:sns:eu-west-1:567589703415:mamip-sns-topic"
+DATE=$(date +%Y-%m-%d-%H-%M)
+WORD_TO_REMOVE="policies/"
+REGION="eu-west-1"
+REPO_URL="git@github.com:z0ph/MAMIP.git"
+REPO_PATH="/tmp/MAMIP"
+S3_KEY_PATH="s3://mamip-artifacts/mamip"
+SSH_KEY_PATH="/tmp/mamip.key"
+GIT_USER_NAME="MAMIP Bot"
+GIT_USER_EMAIL="mamip_bot@github.com"
+SNS_TOPIC_ARN="arn:aws:sns:eu-west-1:567589703415:mamip-sns-topic"
+
+# Function to log messages
+log() {
+    local message="$1"
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $message"
+}
+
+# Function to handle errors
+error_handler() {
+    log "An error occurred. Exiting..."
+    exit 1
+}
+
+# Trap errors
+trap error_handler ERR
 
 # Job preparation (SSH + Git)
-echo "Job preparation"
-aws s3 cp $s3KeyPath $sshKeyPath --region $region
-chmod 600 $sshKeyPath
-eval "$(ssh-agent -s)"
-ssh-add $sshKeyPath
-git config --global user.name "$gitUserName"
-git config --global user.email "$gitUserEmail"
-mkdir -p /home/mamip/.ssh/
-ssh-keyscan github.com >>/home/mamip/.ssh/known_hosts
+prepare_job() {
+    log "Starting job preparation"
+    aws s3 cp "$S3_KEY_PATH" "$SSH_KEY_PATH" --region "$REGION"
+    chmod 600 "$SSH_KEY_PATH"
+    eval "$(ssh-agent -s)"
+    ssh-add "$SSH_KEY_PATH"
+    git config --global user.name "$GIT_USER_NAME"
+    git config --global user.email "$GIT_USER_EMAIL"
+    mkdir -p /home/mamip/.ssh/
+    ssh-keyscan github.com >>/home/mamip/.ssh/known_hosts
+    log "Job preparation completed"
+}
 
-# Clone and process the repo
-echo "Git Clone"
-cd /tmp/
-git clone $repoUrl -q
-if [ -d $repoPath ]; then
-    cd $repoPath
-    echo "Run the magic"
+# Clone the repository
+clone_repo() {
+    log "Cloning the repository"
+    cd /tmp/
+    git clone "$REPO_URL" -q
+    if [ -d "$REPO_PATH" ]; then
+        log "Repository cloned successfully"
+    else
+        log "Failed to clone repository"
+        exit 1
+    fi
+}
+
+# Process the repository
+process_repo() {
+    log "Processing the repository"
+    cd "$REPO_PATH"
     aws iam list-policies --output json >list-policies.json
     jq -cr '.Policies[] | select(.Arn | contains("iam::aws")) | .Arn + " " + .DefaultVersionId + " " + .PolicyName' list-policies.json | xargs -n3 sh -c 'mkdir -p policies && aws iam get-policy-version --policy-arn $1 --version-id $2 > "policies/$3"' sh
+}
 
-    # Push the changes if any
+# Push changes to the repository
+push_changes() {
+    log "Checking for changes"
     if [[ -n $(git status -s) ]]; then
-        diff="$(git diff --name-only) $(git ls-files --others --exclude-standard)"
-        rawDiff=${diff//$wordToRemove/}
-        tweetDiff="${rawDiff:0:200}..."
+        DIFF="$(git diff --name-only) $(git ls-files --others --exclude-standard)"
+        RAW_DIFF=${DIFF//$WORD_TO_REMOVE/}
+        TWEET_DIFF="${RAW_DIFF:0:200}..."
         git add ./policies
         git commit -am "Update detected"
-        git tag $date
-        commit_id=$(git log --format="%h" -n 1)
+        git tag "$DATE"
+        COMMIT_ID=$(git log --format="%h" -n 1)
 
-        # Create JSON message with diff and commit URL
-        message="{\"UpdatedPolicies\": \"$rawDiff\", \"CommitUrl\": \"https://github.com/zoph-io/MAMIP/commit/$commit_id\", \"Date\": \"$date\", \"CommitId\": \"$commit_id\"}"
+        MESSAGE="{\"UpdatedPolicies\": \"$RAW_DIFF\", \"CommitUrl\": \"https://github.com/zoph-io/MAMIP/commit/$COMMIT_ID\", \"Date\": \"$DATE\", \"CommitId\": \"$COMMIT_ID\"}"
+        MESSAGE_BODY="$TWEET_DIFF https://github.com/z0ph/MAMIP/commit/$COMMIT_ID"
 
-        # Craft message for SQS
-        messageBody="$tweetDiff https://github.com/z0ph/MAMIP/commit/$commit_id"
+        aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/567589703415/qtweet-mamip-sqs-queue.fifo" --message-body "$MESSAGE_BODY" --message-group-id 1
+        aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/567589703415/qmasto-development-sqs-queue.fifo" --message-body "$MESSAGE_BODY" --message-group-id 1
+        aws sns publish --topic-arn "$SNS_TOPIC_ARN" --message "$MESSAGE" --region "$REGION"
 
-        # Send messages to SQS queues (Twitter/X + Mastodon)
-        aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/567589703415/qtweet-mamip-sqs-queue.fifo" --message-body "$messageBody" --message-group-id 1
-        aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/567589703415/qmasto-development-sqs-queue.fifo" --message-body "$messageBody" --message-group-id 1
-
-        # Send the message to the SNS topic
-        aws sns publish --topic-arn $snsTopicArn --message "$message" --region $region
-
-        echo "Push the changes to master"
+        log "Pushing changes to master"
         git push origin master --tags
     else
-        echo "No changes detected"
+        log "No changes detected"
     fi
-fi
+}
+
+# Main script execution
+main() {
+    prepare_job
+    clone_repo
+    process_repo
+    push_changes
+    log "Job completed successfully"
+}
+
+main
